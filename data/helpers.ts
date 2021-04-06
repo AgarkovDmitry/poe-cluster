049982076975900789b77c8588596b1eb5bd6247
DimaAgarkov <dioxs.7z@gmail.com>
2021-04-06 19:01:03 +0300

    add images to cluster bases

--- data/helpers.ts
+++ data/helpers.ts
@@ -1,7 +1,7 @@
-import { rawClusterBases, computedNotables } from './data'
+import { computedClusterBases, computedNotables } from './data'
 
 export const getClusterBasesByType = (type: string) =>
-  rawClusterBases.filter((base) => base.master_base === type)
+  computedClusterBases.filter((base) => base.master_base === type)
 
 export const getNotablesByBase = (base: string) =>
-  computedNotables.filter(notable => !!notable.tiers[base])
+  computedNotables.filter((notable) => !!notable.tiers[base])
